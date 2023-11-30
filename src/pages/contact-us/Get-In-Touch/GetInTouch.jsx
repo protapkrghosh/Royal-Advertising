6efee125868203c1151af5c522e6b28cd691957f
Protap Ghosh <protapg518@gmail.com>
2023-11-30 23:43:16 +0600

    added contact info

--- src/pages/contact-us/Get-In-Touch/GetInTouch.jsx
+++ src/pages/contact-us/Get-In-Touch/GetInTouch.jsx
@@ -4,6 +4,10 @@ const GetInTouch = () => {
     <div>
       <div>
         <h3>Contacts</h3>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+
+        </div>
       </div>
     </div>
   );
