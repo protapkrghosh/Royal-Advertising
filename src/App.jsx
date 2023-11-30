644bacef7b113538ec1772b1725f9b60c376f7b2
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2023-11-30 21:43:44 +0600

    react smoothe scrool added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Home from "./pages/home/Home";
+import Footer from "./pages/shered/footer/Footer";
+import Navbar from "./pages/shered/navbar/Navbar";
 
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 };
