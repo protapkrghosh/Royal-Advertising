6f1d986ffd82ce6f41d6947500c8231b1ad4a747
Protap Ghosh <protapg518@gmail.com>
2023-12-01 12:01:39 +0600

    added map in contact us area

--- src/components/contacts/GetInTouch/GetInTouch.jsx
+++ src/components/contacts/GetInTouch/GetInTouch.jsx
@@ -2,44 +2,42 @@ import { IoIosArrowDown } from "react-icons/io";
 
 const GetInTouch = () => {
   return (
-    <div className="">
+    <div>
       {/* Section Heading */}
-      <div className="">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-6xl font-bold mb-7">Contacts</h1>
-          <IoIosArrowDown className="text-2xl" />
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-6xl font-bold mb-7">Contacts</h1>
+        <IoIosArrowDown className="text-2xl" />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-12 gap-10">
+        {/* Email */}
+        <div className="text-center">
+          <div className="flex justify-center">
+            <img src="https://reflectivedesign.in/wp-content/uploads/2023/03/email.png" alt="Email Icon" />
+          </div>
+          <h3 className="text-2xl font-bold my-4">Email</h3>
+          <a href="https://mail.google.com" className="text-[17px] text-[#787C8B] hover:text-[#36325D]">info@refletivedesign.in</a> <br />
+          <a href="https://mail.google.com" className="text-[17px] text-[#787C8B] hover:text-[#36325D]">support@reflectivedesign.in</a>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-12">
-          {/* Email */}
-          <div className="text-center">
-            <div className="flex justify-center">
-            <img src="https://reflectivedesign.in/wp-content/uploads/2023/03/email.png" alt="Email Icon"/>
-            </div>
-            <h3 className="text-2xl font-bold my-4">Email</h3>
-            <a href="https://mail.google.com" className="text-[17px] text-[#787C8B] hover:text-[#36325D]">info@refletivedesign.in</a> <br />
-            <a href="https://mail.google.com" className="text-[17px] text-[#787C8B] hover:text-[#36325D]">support@reflectivedesign.in</a>
+        {/* Email */}
+        <div className="text-center relative">
+          <div className="flex justify-center">
+            <img src="https://reflectivedesign.in/wp-content/uploads/2021/11/map.svg" alt="Address Icon" />
           </div>
+          <h3 className="text-2xl font-bold my-4">Address</h3>
+          <p className="text-[17px] text-[#787C8B] mb-1">Plot No.10, Pratap Nagar, Tedhi Pulia Ring Road, Vikas Nagar, Lucknow, Uttar Pradesh – 226022</p>
+          <a href="" className="text-[17px] text-[#787C8B] hover:text-[#36325D] border-b-2 border-[#787C8B] p-1">Check Location</a>
+        </div>
 
-          {/* Email */}
-          <div className="text-center relative">
-            <div className="flex justify-center">
-              <img src="https://reflectivedesign.in/wp-content/uploads/2021/11/map.svg" alt="Address Icon"/>
-            </div>
-            <h3 className="text-2xl font-bold my-4">Address</h3>
-            <p className="text-[17px] text-[#787C8B] mb-1">Plot No.10, Pratap Nagar, Tedhi Pulia Ring Road, Vikas Nagar, Lucknow, Uttar Pradesh – 226022</p>
-            <a href="" className="text-[17px] text-[#787C8B] hover:text-[#36325D] border-b-2 border-[#787C8B] p-1">Check Location</a>
+        {/* Get In Touch */}
+        <div className="text-center">
+          <div className="flex justify-center">
+            <img src="https://reflectivedesign.in/wp-content/uploads/2021/11/telephone.svg" alt="Telephone Icon" />
           </div>
-
-          {/* Get In Touch */}
-          <div className="text-center">
-            <div className="flex justify-center">
-              <img src="https://reflectivedesign.in/wp-content/uploads/2021/11/telephone.svg" alt="Telephone Icon"/>
-            </div>
-            <h3 className="text-2xl font-bold my-4">Get In Touch</h3>
-            <p className="text-[17px] text-[#787C8B]">+91-8858230920</p>
-            <p className="text-[17px] text-[#787C8B]">+91-8858130920</p>
-          </div>
+          <h3 className="text-2xl font-bold my-4">Get In Touch</h3>
+          <p className="text-[17px] text-[#787C8B]">+91-8858230920</p>
+          <p className="text-[17px] text-[#787C8B]">+91-8858130920</p>
         </div>
       </div>
     </div>
--- src/pages/contactUs/ContactUs.jsx
+++ src/pages/contactUs/ContactUs.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Element } from "react-scroll";
 import GetInTouch from '../../components/contacts/GetInTouch/GetInTouch';
+import Map from '../../components/contacts/Map/Map';
 
 const ContactUs = () => {
   return (
     <Element name="Contacts" className="element">
       <GetInTouch />
+      <Map />
     </Element>
   );
 };
