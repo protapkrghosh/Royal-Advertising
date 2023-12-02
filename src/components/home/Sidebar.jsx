0b404b38748e2229dcfbe2ebee561af4770b3900
Mohammad Rafe <mrrafi5869@gmail.com>
2023-12-02 10:18:21 +0600

    navbar changed

--- src/components/home/Sidebar.jsx
+++ src/components/home/Sidebar.jsx
@@ -1,6 +1,10 @@
-
 import classNames from "classnames";
-import { FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+} from "react-icons/fa";
 const Sidebar = ({ isOpens, onClose }) => {
   const sidebarClasses = classNames("sidebar", {
     "translate-x-0": isOpens,
@@ -8,39 +12,69 @@ const Sidebar = ({ isOpens, onClose }) => {
   });
 
   return (
-    <div className={sidebarClasses} style={{ width: "400px", position: "fixed", top: 0, left: 0, zIndex: 20, transition: "transform 0.3s ease-in-out", boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)" }}>
+    <div
+      className={sidebarClasses}
+      style={{
+        width: "400px",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: 20,
+        transition: "transform 0.3s ease-in-out",
+        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+      }}
+    >
       <nav className="transition-transform duration-300 transform h-screen bg-white  text-[#070234] p-6">
-        <div className="flex justify-between ">
-        <div>
-        <button onClick={onClose} className="absolute top-4 left-4 text-white cursor-pointer bg-red-500 rounded-full p-2">
-          <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-          </svg>
-        </button>
-        </div>
-       <div>
-       <img
-              src="https://b2h9h9q9.rocketcdn.me/wp-content/uploads/2020/11/vishal-mega-mart-Franchise-Logo.png"
-              className="h-12 w-36"
-              alt=""
-            />
-       </div>
+        <div className="flex justify-between items-center">
+          <img
+            src="https://demo.7iquid.com/salepush/wp-content/uploads/2022/04/logo-1.png"
+            className="h-12 w-36"
+            alt=""
+          />
+          <button
+            onClick={onClose}
+            className=" text-white cursor-pointer bg-red-500 rounded-full p-2"
+          >
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            </svg>
+          </button>
         </div>
         <div className="mt-36">
-        <ul className="  items-center ">
-          <li className="flex items-center gap-4 text-[#070234] font-bold my-5 text-xl"><FaFacebook /> Facebook</li>
-          <li className="flex items-center gap-4 text-[#070234] font-bold my-5 text-xl"><FaTwitter /> Twitter</li>
-          <li className="flex items-center gap-4 text-[#070234] font-bold my-5 text-xl"><FaInstagram />Instagram</li>
-          <li className="flex items-center gap-4 text-[#070234] font-bold my-5 text-xl"><FaLinkedinIn />Linkedin</li>
-        </ul>
-
+          <ul className="  items-center ">
+            <li className="flex items-center gap-4 text-[#070234] font-bold my-5 text-xl">
+              <FaFacebook /> Facebook
+            </li>
+            <li className="flex items-center gap-4 text-[#070234] font-bold my-5 text-xl">
+              <FaTwitter /> Twitter
+            </li>
+            <li className="flex items-center gap-4 text-[#070234] font-bold my-5 text-xl">
+              <FaInstagram />
+              Instagram
+            </li>
+            <li className="flex items-center gap-4 text-[#070234] font-bold my-5 text-xl">
+              <FaLinkedinIn />
+              Linkedin
+            </li>
+          </ul>
         </div>
         <div className="mt-10">
-            <hr className="w-full h-1 bg-black"/>
-            <div className="mt-10 text-2xl">
-                <p >+91-8858230920</p>
-                <p className="mt-4">info@reflectivedesign.in</p>
-            </div>
+          <hr className="w-full h-1 bg-black" />
+          <div className="mt-10 text-2xl">
+            <p>+91-8858230920</p>
+            <p className="mt-4">info@reflectivedesign.in</p>
+          </div>
         </div>
       </nav>
     </div>
