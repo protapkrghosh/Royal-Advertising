2a7e7c745231805a4e4af6cc4eabfae74bbc8676
Mohammad Rafe <mrrafi5869@gmail.com>
2023-12-02 10:22:33 +0600

    button

--- src/pages/shered/navbar/Navbar.jsx
+++ src/pages/shered/navbar/Navbar.jsx
@@ -154,7 +154,7 @@ const Navbar = () => {
         </div>
         <button
           type="button"
-          className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          className='nav-button'
         >
           Let's Talk
         </button>
