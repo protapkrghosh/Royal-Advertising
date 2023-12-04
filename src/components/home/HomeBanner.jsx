e6dce6e102d6a38f2f263790baa384af0dd5aeea
Mohammad Rafe <mrrafi5869@gmail.com>
2023-12-04 21:53:33 +0600

    fixed problem

--- src/components/home/HomeBanner.jsx
+++ src/components/home/HomeBanner.jsx
@@ -43,21 +43,11 @@ const HomeBanner = () => {
       <div className="hero mt-24 relative">
         <div className="hero-content flex-col lg:flex-row-reverse justify-center items-center" style={{ zIndex: 1 }}>
           <div>
-            <div className='text-5xl text-white font-bold text-center mt-[150px]'>
-              <p>Boost Your Brand with <br /> Digital Strategies</p>
-              <Typewriter
-                words={['Engagement', 'Visibility', 'Conversion']}
-                loop={true}
-                cursor
-                cursorStyle='_'
-                typeSpeed={70}
-                deleteSpeed={50}
-                delaySpeed={1000}
-                className="text-primary"
-              />
+            <div className='text-7xl text-white font-bold text-center mt-[150px]'>
+              <p>Boost Your Brand with Digital Strategies </p>
             </div>
 
-            <p className="py-6 text-yellow-400 font-semibold text-center mt-6">Give your brand a boost with our digital marketing agency.<br />  We get you a customer not just traffic.</p>
+            <p className="py-6 text-xl text-yellow-400 font-semibold text-center mt-6">Give your brand a boost with our digital marketing agency.<br />  We get you a customer not just traffic.</p>
           </div>
         </div>
       </div>
