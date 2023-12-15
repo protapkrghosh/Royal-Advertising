e1e2d4f2b61ba5b420bc30f1a0bffe9e4fade504
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2023-12-15 15:50:41 +0600

    home page swiper problem solved

--- src/components/home/HomeBanner.jsx
+++ src/components/home/HomeBanner.jsx
@@ -6,7 +6,7 @@ const HomeBanner = () => {
     <div className="relative">
       <img
         src="https://i.ibb.co/bbF23py/download-1.png"
-        className="absolute w-full h-[900px]  md:h-screen object-cover z-[-1]"
+        className="absolute w-full h-[800px]  md:h-screen object-cover z-[-1]"
         alt=""
       />
       
@@ -18,7 +18,7 @@ const HomeBanner = () => {
           className="hero-content flex-col lg:flex-row-reverse justify-center items-center"
           style={{ zIndex: 1 }}
         >
-          <div className=' lg:ml-[63px] md:mt-36 lg:mt-14 mt-40  text-center '>
+          <div className=' lg:ml-[63px] md:mt-36 lg:mt-14 mt-32  text-center '>
          <h1 className='text-[#070234] lg:text-6xl text-4xl font-bold'>Unlock the Power of <br /> Outdoor Brilliance</h1>
          <p className='text-white md:text-lg mt-4'>Crafting Impressions, Connecting Spaces. <br /> Welcome to Royal Advertising</p>
           </div>
