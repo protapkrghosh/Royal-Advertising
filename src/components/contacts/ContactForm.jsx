b9395e4942098267d560744dd665b60af65593f2
Mohammad Rafe <mrrafi5869@gmail.com>
2023-12-18 19:32:57 +0600

    email js

--- src/components/contacts/ContactForm.jsx
+++ src/components/contacts/ContactForm.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import contact from "../../data/animations/Contact.json"
 import Lottie from "lottie-react";
 import useAOSInit from "../../hooks/useAosInit";
-
+import emailjs from '@emailjs/browser';
 const ContactForm = () => {
   useAOSInit()
+  const form = useRef();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+    emailjs.sendForm('service_pvmroq8', 'template_7hxjdqo', form.current, 'WoZaV_ZGijOfT8x-7')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <div className="mt-24">
       <div>
@@ -22,37 +34,37 @@ const ContactForm = () => {
 
           {/* Contact Form */}
           <div data-aos="fade-left" data-aos-duration="3000" className="mt-20  ">
-            <form action="">
+            <form  ref={form} onSubmit={sendEmail}>
               <div className="flex justify-center gap-3">
                 <div className="flex flex-col w-full">
                   <label htmlFor="name" className="text-[18px] text-[#787C8B]">Name <span className="text-[#F51843]">*</span></label>
-                  <input type="text" id="name" className="outline-none border-b border-[#69727D] focus:border-[#D8D9E5] w-full max-w-xs p-1" />
+                  <input type="text" id="name" name='from_name' className="outline-none border-b border-[#69727D] focus:border-[#D8D9E5] w-full max-w-xs p-1" />
                 </div>
                 <div className="flex flex-col w-full">
                   <label htmlFor="email" className="text-[18px] text-[#787C8B]">Email <span className="text-[#F51843]">*</span></label>
-                  <input type="text" id="email" className="outline-none border-b border-[#69727D] focus:border-[#D8D9E5] w-full max-w-xs p-1" />
+                  <input type="text" id="email" name='to_email' className="outline-none border-b border-[#69727D] focus:border-[#D8D9E5] w-full max-w-xs p-1" />
                 </div>
               </div>
 
               <div className="flex justify-center gap-3 my-3">
                 <div className="flex flex-col w-full">
                   <label htmlFor="phone" className="text-[18px] text-[#787C8B]">Phone <span className="text-[#F51843]">*</span></label>
-                  <input type="text" id="phone" className="outline-none border-b border-[#69727D] focus:border-[#D8D9E5] w-full max-w-xs p-1" />
+                  <input type="text" id="phone" name='phone' className="outline-none border-b border-[#69727D] focus:border-[#D8D9E5] w-full max-w-xs p-1" />
                 </div>
                 <div className="flex flex-col w-full">
                   <label htmlFor="services" className="text-[18px] text-[#787C8B]">Services <span className="text-[#F51843]">*</span></label>
-                  <input type="text" id="services" className="outline-none border-b border-[#69727D] focus:border-[#D8D9E5] w-full max-w-xs p-1" />
+                  <input type="text" id="services" name='services' className="outline-none border-b border-[#69727D] focus:border-[#D8D9E5] w-full max-w-xs p-1" />
                 </div>
               </div>
 
               <div>
                 <div className="flex flex-col w-full">
                   <label htmlFor="phone" className="text-[18px] text-[#787C8B]">Message <span className="text-[#F51843]">*</span></label>
-                  <textarea cols="5" rows="4" className="outline-none border-b border-[#69727D] focus:border-[#D8D9E5] w-full p-1"></textarea>
+                  <textarea cols="5" rows="4" name='message' className="outline-none border-b border-[#69727D] focus:border-[#D8D9E5] w-full p-1"></textarea>
                 </div>
               </div>
-
-              <input type="submit" value="Send Message" className="btn text-[17px] font-bold rounded-full bg-[#FF2E57] hover:bg-[#F51843] px-6 text-white mt-6"></input>
+              <button className="btn text-[17px] font-bold rounded-full bg-[#FF2E57] hover:bg-[#F51843] px-6 text-white mt-6">Send Message</button>
+              {/* <input type="submit" value="Send Message" className="btn text-[17px] font-bold rounded-full bg-[#FF2E57] hover:bg-[#F51843] px-6 text-white mt-6"></input> */}
             </form>
 
 
