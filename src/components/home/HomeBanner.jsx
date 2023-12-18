9e72031cf0711d5bf56e464e22656ec71a2f8fcc
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2023-12-18 15:13:11 +0600

    background changed

--- src/components/home/HomeBanner.jsx
+++ src/components/home/HomeBanner.jsx
@@ -3,31 +3,19 @@ import Navbar from "../../pages/shered/navbar/Navbar";
 
 const HomeBanner = () => {
   return (
-    <div className="relative">
-<video
-        autoPlay
-        muted
-        loop
-        className="absolute w-full h-[800px] md:h-screen object-cover z-[-1]"
-      >
-        <source
-          src="https://media.istockphoto.com/id/521188514/video/traffic-car-on-busy-highway-near-to-the-skyscrapers-in-madrid.mp4?s=mp4-480x480-is&k=20&c=iAVRswaweN0Y1xkHMYqCxB0XnbVh9fYuMx3r6UQD5fo=" // Replace with the URL of your video file
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
+    <div className="relative "> 
+      <img src="https://cdn-edjcg.nitrocdn.com/VBWSxyKIMetGynBOPkcprlqWleWuyTij/assets/images/optimized/rev-9f1d91c/www.blipbillboards.com/wp-content/uploads/image-11-1.png" className='w-full h-screen' alt="" />
 
-      {/* Navbar Component */}
-      <Navbar />
-      <div className="hero  mt-24 relative">
+      
+      <div className="  absolute top-[210px] left-[20px]  md:top-[230px] md:left-[200px] ">
         <div
-          className="hero-content flex-col lg:flex-row-reverse justify-center items-center"
+          className=""
           style={{ zIndex: 1 }}
         >
-          <div className='  md:mt-36 lg:mt-0 mt-24  text-center '>
-         <h1 className='text-[#070234] lg:text-6xl text-4xl font-bold'>Unlock the Power of <br /> Outdoor Brilliance</h1>
-         <p className='text-[#291E97] md:text-xl md:mt-8 mt-6 font-semibold'>Crafting Impressions, Connecting Spaces. <br /> Welcome to Royal Advertising</p>
-         <button type="button" className="btn-navbar mt-2">
+          <div className=' '>
+         <h1 className='text-[#FFF] lg:text-6xl text-4xl font-bold'>Unlock the Power of <br /> Outdoor Brilliance</h1>
+         <p className='text-[#FFF] md:text-xl mt-6 font-semibold'>Welcome to Royal Advertising, where we redefine the art of <br /> outdoor promotion.  With a commitment to excellence and a  <br />  touch of royalty, we craft bespoke advertising solutions <br />  that elevate your brand to new heights</p>
+         <button type="button" className="btn-navbar mt-4">
           Let's Talk
         </button>
           </div>
