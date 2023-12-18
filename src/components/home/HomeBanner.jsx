db1dfbc2800531ade8c276e9978c0b3d26a87a13
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2023-12-18 16:42:28 +0600

    navbar spacing added

--- src/components/home/HomeBanner.jsx
+++ src/components/home/HomeBanner.jsx
@@ -7,7 +7,7 @@ const HomeBanner = () => {
       <img src="https://cdn-edjcg.nitrocdn.com/VBWSxyKIMetGynBOPkcprlqWleWuyTij/assets/images/optimized/rev-9f1d91c/www.blipbillboards.com/wp-content/uploads/image-11-1.png" className='w-full h-screen' alt="" />
 
       
-      <div className="  absolute top-[210px] left-[20px]  md:top-[230px] md:left-[200px] ">
+      <div className="  absolute top-[210px] left-[20px]  lg:top-[230px] lg:left-[150px] ">
         <div
           className=""
           style={{ zIndex: 1 }}
