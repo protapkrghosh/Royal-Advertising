436e338687da1edce4f4a84b5cf313db3905df08
Protap Ghosh <protapg518@gmail.com>
2023-12-20 14:23:07 +0600

    update image in services section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
          backgroundImage: {
             aboutus: "url('./src/assets/aboutUs.jpg')",
             whyUs: "url('./src/assets/whyus.jpg')",
-            servicesDescriptions: "url('./src/assets/servicesDescriptions.jpg')",
+            servicesDescriptions: "url('https://i.ibb.co/7S22kzL/services-Descriptions.jpg')",
             whyChouseUs: "url('./src/assets/whyChooseUs.jpg')",
          },
       },
